639e42b56483db9944f685ee641bdb9ff08e221c
Shane <shane8641@live.com>
2015-10-16 23:23:25 -0400

    Added Car class and getters/setters

--- Car_Accelerate_Brake_App/Car.cs
+++ Car_Accelerate_Brake_App/Car.cs
@@ -8,6 +8,34 @@ namespace Car_Accelerate_Brake_App
 {
     class Car
     {
-
+        // Fields
+        private int _year;
+        private string _make;
+        private int _speed;
+        // Constructor
+        public Car(int yr,string mk) 
+        {
+            _speed = 0;
+            _year = yr;
+            _make = mk;
+        }
+        // Year property
+        public int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+        // Make property
+        public string Make
+        {
+            get { return _make; }
+            set { _make = value; }
+        }
+        // Speed property
+        public int Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
     }
 }
