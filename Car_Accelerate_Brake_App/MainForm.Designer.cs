2906c2e04a8abb7efb4fe0fe319e9d244bd5aa3c
Shane <shane8641@live.com>
2015-10-16 21:44:05 -0400

    Added Car class and renamed form1

--- Car_Accelerate_Brake_App/MainForm.Designer.cs
+++ Car_Accelerate_Brake_App/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Car_Accelerate_Brake_App
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(916, 592);
+            this.Name = "Form1";
+            this.Text = "Car Application";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Car_Accelerate_Brake_App/MainForm.cs
+++ Car_Accelerate_Brake_App/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Car_Accelerate_Brake_App
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
